39057f7c8f8789d4ba6467412d745db86f0e41fd
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2024-07-19 10:14:49 -0700

    Update Program.cs

--- MSIFIC/Program.cs
+++ MSIFIC/Program.cs
@@ -11,35 +11,45 @@ namespace ficapptest
     {
         static async Task Main(string[] args)
         {
-            string appClientId = "60416d10-5297-4d99-8ec8-9d0cbca81fbc";
-            string resourceTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+            // Define application and resource details
+            string appClientId = "60416d10-5297-4d99-8ec8-9d0cbca81fbc"; //replace with your Azure AD App registration Client ID
+            string resourceTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"; //This is MSFT tenant ID - Do not change this
             Uri authorityUri = new($"https://login.microsoftonline.com/{resourceTenantId}");
-            string miClientId = "de7665da-cbfa-4b05-b4e6-5867195e6443";
-            string audience = "api://AzureADTokenExchange";
+            string miClientId = "de7665da-cbfa-4b05-b4e6-5867195e6443"; //replace with the UAMI you created 
+            string audience = "api://AzureADTokenExchange"; //This is the Token Exchange scope
             string vaultUrl = "https://queryme.vault.azure.net";
             string secretName = "secret";
 
             Console.WriteLine("Starting token acquisition process...");
             Console.WriteLine("");
+            
             // Gets a token for the user-assigned Managed Identity.
+            // Delegate to acquire token for the user-assigned Managed Identity
             var miAssertionProvider = async (AssertionRequestOptions _) =>
             {
                 Console.WriteLine("Acquiring token for user-assigned Managed Identity...");
                 Console.WriteLine("");
 
+                // Create Managed Identity application
                 var miApplication = ManagedIdentityApplicationBuilder
                     .Create(ManagedIdentityId.WithUserAssignedClientId(miClientId))
                     .Build();
 
+                //Federated Identity Credentials allow applications to use access tokens issued by trusted identity providers as credentials.
+                //The audience value "api://AzureADTokenExchange" indicates that the token is meant for the Azure AD Token Exchange service, which is a part of the Federated Identity Credentials flow
                 var miResult = await miApplication.AcquireTokenForManagedIdentity(audience)
                     .ExecuteAsync()
                     .ConfigureAwait(false);
 
                 Console.WriteLine($"Managed Identity token acquired: {miResult.AccessToken}");
                 Console.WriteLine("");
+
+                //This token from the Managed Identity, can now be exchanged for another token from AAD that can be used to access the Key Vault.
+                //See line 55 where the MI Token is used as an Assertion and is exchanged with ESTS to get a AAD token
                 return miResult.AccessToken;
             };
 
+            // Configure the confidential client application with Managed Identity assertion
             IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(appClientId)
               .WithAuthority(authorityUri, false)
               .WithClientAssertion(miAssertionProvider)
@@ -50,6 +60,8 @@ namespace ficapptest
 
             Console.WriteLine("Acquiring token for confidential client...");
             Console.WriteLine("");
+
+            // Acquire token for the confidential client
             AuthenticationResult result = await app.AcquireTokenForClient(scopes)
               .ExecuteAsync()
               .ConfigureAwait(false);
